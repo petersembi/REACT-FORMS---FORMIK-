7943cbde5f84588ed2f40962d8d893bee4a6f232
petersembi <petersembi@gmail.com>
2024-02-07 09:04:04 +0300

    initial commit

---
+++ src/components/RootComponent.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom'
+import SignInComponent from './SignInComponent';
+
+class RootComponent extends React.Component {
+    render() {
+        return (
+            <Router>
+                <Switch>
+                    <Route 
+                        exact
+                        path={"/signin"}
+                        render={ () => { return <SignInComponent />; }}
+                    />
+                </Switch>
+            </Router>
+        );
+    }
+}
+
+export default RootComponent;
